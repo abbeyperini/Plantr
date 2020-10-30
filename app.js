389e2c4f0e112445f4440f0ea7f8a2e3c17dbfd7
abbeyperini <abbeyperini@gmail.com>
2020-10-30 11:43:13 -0400

    app.js, routes

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
 const PORT = process.env.PORT || 8080
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
+const mustacheExpress = require('mustache-express');
+const session = require('express-session');
+const { dirname } = require('path');
+const VIEW_PATH = path.join(__dirname, '/views');
+const models = require('./models');
+const { Op } = require('sequelize');
+const { allowedNodeEnvironmentFlags } = require('process');
+const indexRoutes = require('./routes/index');
+const accountRoutes = require('./routes/account');
+
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+}));
+
+app.use(bodyParse.urlencoded({extended: false}));
+app.use('/styles', express.static('styles'));
+app.use('/images', express.static('images'));
+app.engine('mustache', mustacheExpress(VIEWS_PATH + '/partials', '.mustache'));
+app.set('views', VIEWS_PATH);
+app.set('view engine', 'mustache');
+app.use('/index', indexRoutes);
+app.use('/account', accountRoutes);
+
+app.listen(3000, () => {
+    console.log("Server is running...");
+});
---
+++ routes/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const mustacheExpress = require('mustache-express');
+const router = express.Router();
+
+app.engine('mustache', mustacheExpress());
+app.set('views', './views');
+app.set('view engine', 'mustache');
+module.exports = router;
