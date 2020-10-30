c6c14323357d60a013e6d67b12f1a835e2452a24
Cristina Hernandez <64177857+cristinahdz29@users.noreply.github.com>
2020-10-30 17:02:34 -0400

    created update and delete actions for plants

--- routes/account.js
+++ routes/account.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const mustacheExpress = require('mustache-express');
 const router = express.Router();
+const models = require('../models')
 
 app.engine('mustache', mustacheExpress());
 app.set('views', './views');
@@ -50,7 +51,56 @@ router.post('/create-plant', (req, res) => {
     
     plant.save().then((savedPlant) => {
         console.log(savedPlant)
-        res.redirect('/')
+        res.redirect('/account')
     })
 })
 
+//delete route
+router.post('/delete-plant', (req, res)=> {
+    const plant_id = req.body.plant_id
+
+    models.Plants.destroy({
+        where: {
+            id: plant_id
+        }
+    }).then((deletedPlant) => {
+        res.redirect('/account')
+    })
+})
+
+//update plant
+router.get('/edit/:id', (req, res)=> {
+    const plant_id = parseInt(req.params.id)
+    console.log(plant_id)
+
+    models.Plants.findByPk(plant_id).then((plant)=> {
+        res.render('update-plant', plant.dataValues)
+    }).catch(error => {
+        console.log(error)
+    })
+})
+
+router.post('/update-plant', (req, res)=> {
+    const common_name = req.body.common_name;
+    const scientific_name = req.body.scientific_name;
+    const watering_schedule = req.body.watering_schedule;
+    const light_requirement = req.body.light_requirement;
+    const soil_type = req.body.soil_type;
+    const plant_id = req.body.plant_id
+    
+
+    //creating plant object
+    models.Plants.update({
+      common_name: common_name,
+      scientific_name: scientific_name,
+      watering_schedule: watering_schedule,
+      light_requirement: light_requirement,
+      soil_type: soil_type
+    }, {
+        where: {
+            id: plant_id
+        }
+    }).then(updatedPlant => {
+        res.redirect('/account')
+    })
+})
