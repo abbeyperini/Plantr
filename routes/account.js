54a2d43064ace79eda4d6e7615e60bb4473a7d6e
abbeyperini <abbeyperini@gmail.com>
2020-11-01 15:03:31 -0500

    design/styling

--- routes/account.js
+++ routes/account.js
@@ -9,19 +9,6 @@ app.set('views', './views');
 app.set('view engine', 'mustache');
 module.exports = router;
 
-
-//LAYOUT PLAN:
-//----------------------------------------------------------------//
-//account routers
-//Routes needed for account:
-// /account --see all your posts
-// /account/create-post --add a post to your profile
-// /account/delete-post --delete a post from your profile
-// /account/edit-post --edit/update a post on your profile
-// /account/profilenewsfeed -- see posts from other users
-
-//----------------------------------------------------------------//
-
 //see profile page that displays all posts
 router.get('/', (req, res) => {
     let userId = req.session.userId;
